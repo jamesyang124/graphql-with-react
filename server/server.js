d135090671e1ddd94fc7d26a8dbd7f466f43dae0
James Yang <jamesyang124@gmail.com>
2018-04-17 23:56:10 +0800

    UPD. enable CORS for GraphQL server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import {graphqlExpress, graphiqlExpress} from 'graphql-server-express';
 import bodyParser from 'body-parser';
 
 import {schema} from './src/schema';
 
 const PORT = 4000;
+const CLIENT_APP_ORIGIN = "http://localhost:3000";
+
 const server = express();
 
-server.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+var corsOptions = {
+  origin: CLIENT_APP_ORIGIN,
+  optionsSuccessStatus: 200
+  // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+server.use('/graphql', cors(corsOptions), bodyParser.json(), graphqlExpress({schema}));
 
 server.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
